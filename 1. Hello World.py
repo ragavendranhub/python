efdc336dab0916135267b13a194cc0c57be34ab1
Ragavendran S <80335220+ragavendranhub@users.noreply.github.com>
2021-04-01 22:38:32 +0530

    Add files via upload

---
+++ 1. Hello World.py
@@ -0,0 +1,12 @@
+# Comments are starting with '#'
+
+#print() is a function, which display the value of a variable, are string passed within
+print ("Hello, World!")
+
+#Strings are surrounded by "" or ''
+a="Wonder"
+a1=" Woman"
+b=1984
+
+#to add two strings, we use '+' sign here
+print(a + a1, b)
